88fa283056b248f704127b74b70cff2e33d30612
tomarvansh9582-ai <tomarvansh9582@gmail.com>
2026-07-03 15:49:03 +0530

    Add files via upload

---
+++ 3-07-2026(2).py
@@ -0,0 +1,24 @@
+for i in range(5):
+    print("Hello!")
+    
+# Print numbers 0 through 4
+for i in range(5):
+    print(i)
+
+# Output:
+# 0
+# 1
+# 2
+# 3
+# 4
+count = 0
+while count < 5:
+    print(f"Count is {count}")
+    count = count + 1  # Increase count by 1
+
+# Output:
+# Count is 0
+# Count is 1
+# Count is 2
+# Count is 3
+# Count is 4
